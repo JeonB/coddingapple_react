6af813a0b2e49a6b39ae659cc6cc07332ca96baf
JeonB <newjeonb@gmail.com>
2023-11-16 21:40:41 +0900

    Add Modal component

--- src/App.js
+++ src/App.js
@@ -8,34 +8,83 @@ function App() {
   let [글제목, b] = useState("햄버거 메뉴 추천");
   //온갖거 스테이트 사용 X
   let [logos, setBlog] = useState(["블로그임 ㅎㅇ", "리액트독학", "뭐먹지"]);
+
   let [like, setLike] = useState(0);
   return (
     <div className="App">
-      {/* 상단 nav */}
       <div className="black-nav">
-        <h4 style={{ color: "red", fontSize: "36px" }}>{logos}</h4>
+        <h4 style={{ color: "white", fontSize: "36px" }}>{글제목}</h4>
       </div>
-      {logos.map((logo) => {
-        return (
-          <div className="list">
-            <h4>
-              {logo}
-              <span
-                onClick={() => {
-                  setLike(like + 1);
-                }}
-                style={{ cursor: "pointer" }}
-              >
-                👍
-              </span>
-              {like}
-            </h4>
-            <p>11월 15일 발행</p>
-          </div>
-        );
-      })}
+      <button
+        onClick={() => {
+          let copy = [...logos].sort((a, b) => a.localeCompare(b, "ko"));
+          setBlog(copy);
+        }}
+      >
+        가나다순정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...logos]; // ... 객체를 가져오는 문법(["블로그임 ㅎㅇ", "리액트독학", "뭐먹지"]). 이 때, 대괄호가 해제되면서 배열의 주소도 달라짐.(destructuring) deep copy
+          if (copy[0] === "카페야 꺼져") copy[0] = "블로그임";
+          else copy[0] = "카페야 꺼져";
+          setBlog(copy);
+        }}
+      >
+        눌러봥
+      </button>
+      <div className="list">
+        <h4>
+          {logos[0]}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            👍
+          </span>
+          {like}
+        </h4>
+        <p>11월 15일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>
+          {logos[1]}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+            style={{ cursor: "pointer" }}
+          ></span>
+        </h4>
+        <p>11월 15일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {logos[2]}
+          <span
+            onClick={() => {
+              setLike(like + 1);
+            }}
+            style={{ cursor: "pointer" }}
+          ></span>
+        </h4>
+        <p>11월 15일 발행</p>
+      </div>
+      <Modal />
     </div>
   );
 }
 
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
 export default App;
